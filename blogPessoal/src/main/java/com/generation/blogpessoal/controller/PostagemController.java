bba8547f89bbabdb57a5cdb2adc5cad4fd9883fd
Alessandro Castro <97323030+alecfonseca@users.noreply.github.com>
2022-02-18 21:12:07 -0300

    crudparte1

--- blogPessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,10 +2,18 @@ package com.generation.blogpessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +22,70 @@ import com.generation.blogpessoal.repository.PostagemRepository;
 
 @RestController
 @RequestMapping("/postagens")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*") // deixa eu rodar origens de lugares diferentes
 public class PostagemController {
-	
-	@Autowired
-	private PostagemRepository postagemRepository;
-	
-	@GetMapping
-	public ResponseEntity<List<Postagem>> getAll(){
-		return ResponseEntity.ok(postagemRepository.findAll());
-		
-	}
-	
 
+    @Autowired // da acesso ao meu controller a responsabilidade de criar e instanciar objetos
+    private PostagemRepository postagemRepository;
+    
+    @GetMapping //n podem existir 2 getmapping iguais
+    public ResponseEntity<List<Postagem>> getAll() {
+        return ResponseEntity.ok(postagemRepository.findAll());
+    } // equivalente a fazer = select * from tb_postagens;
+    
+    @GetMapping("/{id}") //variavel de caminho
+    public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+        return postagemRepository.findById(id) //procura pelo id 
+                .map(resposta -> ResponseEntity.ok(resposta)) //realiza se resposta n for nulla
+                .orElse(ResponseEntity.notFound().build()); //realiza se a resposta for nulla
+                //Lambda = (resposta -> oqueretorna)
+    }
+    
+    @GetMapping("/titulo/{titulo}") //n podem existir 2 getmapping iguais
+    public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+                            //o List nÃ£o gera erro igual findById pois ele n gera null, gera uma lista vazia
+        return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+    } //select * from tb_postagens where titulo like "%titulo%";
+    
+    @PostMapping
+    public ResponseEntity<Postagem> postPostagem (@Valid @RequestBody Postagem postagem) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+        // chamo o status Created e no corpo do meu status eu salvo o objeto postagem e recebo o ok
+    }
+    
+    @PutMapping
+    public ResponseEntity<Postagem> putPostagem (@Valid @RequestBody Postagem postagem) {
+        //return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+        return postagemRepository.findById(postagem.getId()) //procura pelo id 
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem))) //realiza se resposta n for nulla
+                .orElse(ResponseEntity.notFound().build()); //realiza se a resposta for nulla            
+       /* 	@PutMapping
+    			public ResponseEntity<Postagem> putPostagem (@Valid @RequestBody Postagem postagem){
+        			return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+
+    }*/
+    }
+    @DeleteMapping("/{id}")
+	public ResponseEntity<Object> deletePostagem(@PathVariable Long id) { // postagemRepository.deleteById(id);
+		return postagemRepository.findById(id).map(resposta -> {
+			postagemRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}).orElse(ResponseEntity.notFound().build());
+	}
+    /*	@DeleteMapping("/{id}")
+    *	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+    *	public void deletePostagem(@PathVariable Long id) { 
+    *		Optional<Postagem> postagem = postagemRepository.findById(id);
+    *		if (postagem.isEmpty())
+    *				throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    *		
+    *		postagemRepository.deleteById(id);
+    *	}
+    */	
+    	
+    	
+//    	@DeleteMapping("/{id}") /*ele n retorna nada, é do tipo void*/
+//    	public void  deletePostagem(@PathVariable Long id) { 
+//    		postagemRepository.deleteById(id); /*método de apagar tá pronto!*/
+//    	} 
 }
